f69dca6f5615e2d88f3c4bdf00634d41b18a5bb7
everwhatever <koszykarz.kuba@gmail.com>
2022-06-08 07:57:01 +0200

    upgrade to symfony6, graphql issue

--- app/rector.php
+++ app/rector.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Rector\Symfony\Set\SymfonyLevelSetList;
 use Rector\Symfony\Set\SymfonySetList;
 use Rector\Config\RectorConfig;
 
@@ -9,7 +10,7 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
 
     $rectorConfig->sets([
-        \Rector\Symfony\Set\SymfonyLevelSetList::UP_TO_SYMFONY_54,
+        SymfonyLevelSetList::UP_TO_SYMFONY_60,
         SymfonySetList::SYMFONY_CODE_QUALITY,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
     ]);
